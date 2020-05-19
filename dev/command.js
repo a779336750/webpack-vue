791efadc93d786bb6436f575280cb3fa30f96f3d
jason <779336750@qq.com>
2020-05-19 16:46:41 +0800

    新增express服务检测

--- dev/command.js
+++ dev/command.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
-const { program } = require('commander');
+const {program} = require('commander');
 const _ = require('lodash');
 const chalk = require('chalk');
-const _vconf = require('./config/vconf');
+const _vconf = require('../config/vconf');
 const {exec} = require('child_process');
-
+const net = require('net');
 const success = chalk.green;
 const info = chalk.blue;
 const error = chalk.bold.red;
@@ -12,24 +12,58 @@ const error = chalk.bold.red;
 // 定义一些指令集合
 const command = {
     Webpack: 'webpack-dev-server --open --config',
-    RunExpress: 'node server/dev.js',
+    RunExpress: 'node dev/express.js',
 };
+
+function checkExpress() {
+    const port = _vconf.ExpressPort;
+    return new Promise((resolve,reject) => {
+        const server = net.createServer().listen(port);
+        server.on('listening', () => {
+            server.close();
+            resolve();
+        });
+
+        // 端口已经被使用
+        server.on('error', err => {
+            if (err.code === 'EADDRINUSE') {
+                reject();
+            }
+        });
+    })
+}
+
+function runExpress() {
+    checkExpress().then(() => {
+        console.log(success('---------启动Express服务---------'));
+        const cmd = command.RunExpress;
+        const workerProcess = exec(cmd,()=>{});
+        workerProcess.stdout.on('data', data => {
+            console.log(data);
+        });
+        workerProcess.stderr.on('data', data => {
+            console.log(data);
+        });
+    }).catch(error=>{
+        console.log(error);
+    });
+}
 
 function execWebpack(options) {
     const commandObj = validateWebpack(options);
-    if(!commandObj) {
+    if (!commandObj) {
         process.exit();
         return;
     }
-    const { cmd, nodeEnv, devEnv } = commandObj;
+    const {cmd, nodeEnv, devEnv} = commandObj;
     console.log(success('---------启动Webpack服务---------'));
     console.log(info('---------------------------------'));
     console.log(info(`---------部署环境为:${nodeEnv}---------`));
     console.log(info(`---------服务器环境为:${devEnv}---------`));
 
     // 设置环境变量
-    process.env.NODE_ENV =nodeEnv;
-    process.env.DEV_ENV =devEnv;
+    process.env.NODE_ENV = nodeEnv;
+    process.env.DEV_ENV = devEnv;
     console.log(info(`---------运行：${cmd}---------`));
 
     const workerProcess = exec(cmd);
@@ -60,18 +94,18 @@ function buildWebpack() {
 }
 
 function validateWebpack(options) {
-    const {nodeEnv,devEnv} = options;
+    const {nodeEnv, devEnv} = options;
 
-    if(_.indexOf(_vconf.NodeEnvs,nodeEnv) < 0) {
+    if (_.indexOf(_vconf.NodeEnvs, nodeEnv) < 0) {
         console.error(error(`##部署环境错误，有效值为：${_vconf.NodeEnvs}`));
         return false;
     }
-    if(_.indexOf(_vconf.DevEnvs,devEnv) < 0) {
+    if (_.indexOf(_vconf.DevEnvs, devEnv) < 0) {
         console.error(error(`##服务器环境错误，有效值为：${_vconf.DevEnvs}`));
         return false;
     }
-    const envType =nodeEnv === 'prod'?'prod':'dev';
-    const isWatch =nodeEnv === 'dev'?'--watch':'';
+    const envType = nodeEnv === 'prod' ? 'prod' : 'dev';
+    const isWatch = nodeEnv === 'dev' ? '--watch' : '';
     const cmd = `${command.Webpack} webpack\\ktu.${envType}.js`;
     return {
         nodeEnv,
@@ -79,16 +113,18 @@ function validateWebpack(options) {
         cmd,
     }
 }
+
 program
     .command('run')
     .alias('r')
     .description('运行项目')
-    .option('-O, --open','启动web服务')
-    .option('-N,--node-env <itemName>','部署环境变量','dev')
-    .option('-D,--dev-env <itemName>','服务器环境变量','dev')
+    .option('-O, --open', '启动web服务')
+    .option('-N,--node-env <itemName>', '部署环境变量', 'dev')
+    .option('-D,--dev-env <itemName>', '服务器环境变量', 'dev')
     .action(function (option, otherD, cmd) {
-        if(option.open) {
+        if (option.open) {
             console.log('启动web服务');
+            runExpress();
         }
         execWebpack({
             nodeEnv: option.nodeEnv,
@@ -103,10 +139,7 @@ program.on('--help', function (operands) {
     console.log('');
     console.log('执行示例');
     console.log('');
-    console.log('$ ktu run -P compress');
-    console.log('$ ktu run -O -P compress');
-    console.log('$ ktu run -O -P compress -N prod');
-    console.log('$ ktu run -O -P compress -N prod -D prod');
+    console.log('$ we run -O ');
 });
 
 program.command('build')
