2c643ff3d6397e4a8d049c5eff6ba2dddf8020db
jason <779336750@qq.com>
2020-05-19 16:46:41 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import App from './components/index.vue'
 import VueVirtualScroller from 'vue-virtual-scroller'
 Vue.config.productionTip = false
 Vue.use(VueVirtualScroller)
-
 new Vue({
     render: h => h(App),
 }).$mount('#app')
