c68066b50caba187bdc31b1149a2d934cd8a06cf
json <779336750@qq.cpm>
2020-06-08 23:56:25 +0800

    test2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import App from './components/index.vue'
 import VueVirtualScroller from 'vue-virtual-scroller'
 Vue.config.productionTip = false
 Vue.use(VueVirtualScroller)
+
 new Vue({
     render: h => h(App),
 }).$mount('#app')
